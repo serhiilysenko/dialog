7b6d69569793f44a0eb453e747c17347a80f6bde
serhiilysenko <diom34@gmail.com>
2020-12-15 17:26:41 +0200

    static dialog

---
+++ src/components/Dialog/helpers.js
@@ -0,0 +1,39 @@
+import dialog from "../../data";
+
+export const normalizeDialog = (dialog) => {
+    const newDialog = [];
+
+    dialog.forEach((item, i) => {
+        if (i === 0 ||item.is !== dialog[i-1].is) {
+            newDialog.push({
+                type: "message",
+                id: `item-message-${item.id}`,
+                avatar: item.avatar,
+                isReverse: item.is === "my",
+                isRemovale: item.is === "my",
+                messages: [
+                    {
+                        text: item.message,
+                        status: item.status,
+                        id: item.id,
+                        date: item.date,
+                    },
+                ],
+            });
+        } else {
+            const position = newDialog.length - 1;
+
+            newDialog[position] = {
+                ...newDialog[position],
+                messages: newDialog[position].messages.concat({
+                    text: item.message,
+                    status: item.status,
+                    id: item.id,
+                    date: item.date,
+                }),
+            };
+        }
+    });
+
+    return newDialog;
+};
---
+++ src/components/Header/UserInfo/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+const UserInfo = () => (
+    <div className="user-info">
+        <div className="name">Lezus</div>
+        <div className="status">
+            <span className="circle" />Online
+        </div>
+    </div>
+);
+
+export default UserInfo;
